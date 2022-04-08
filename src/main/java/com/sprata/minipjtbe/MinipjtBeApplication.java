4c96d34aa5d01393df95854db7b9f2a9b3515112
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-04-08 10:35:32 +0900

    시작!

---
+++ src/main/java/com/sprata/minipjtbe/MinipjtBeApplication.java
@@ -0,0 +1,13 @@
+package com.sprata.minipjtbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinipjtBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinipjtBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprata/minipjtbe/MinipjtBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprata.minipjtbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinipjtBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
