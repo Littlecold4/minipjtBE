569166c6dffb6f11d73bf23bf435f0e26cab23d7
HyungbinK <toay145@gmail.com>
2022-04-09 22:11:50 +0900

    Comment Timestamped

--- src/main/java/com/sprata/minipjtbe/dto/CommentResponseDto.java
+++ src/main/java/com/sprata/minipjtbe/dto/CommentResponseDto.java
@@ -4,6 +4,8 @@ import com.sprata.minipjtbe.model.Comment;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 public class CommentResponseDto {
@@ -11,12 +13,16 @@ public class CommentResponseDto {
     private UserInfoDto userInfo;
     private Long boardId;
     private String comment;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
 
     public CommentResponseDto(Comment comment, UserInfoDto userInfo) {
         this.userInfo = userInfo;
         this.boardId = comment.getBoardId();
         this.comment = comment.getComment();
+        this.createdAt = comment.getCreatedAt();
+        this.modifiedAt = comment.getModifiedAt();
 
     }
 
